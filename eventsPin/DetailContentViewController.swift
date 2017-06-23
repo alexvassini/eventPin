98f315b98cd5e246b65e9f3076312555550bc1e4
Alexandre  Vassinievski <alexvassini@gmail.com>
2017-06-23 19:23:05 -0300

    pageViewController implemented
    bug: AppCrash on device rotate

---
+++ eventsPin/DetailContentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailContentViewController.swift
+//  eventsPin
+//
+//  Created by Alexandre  Vassinievski on 23/06/17.
+//  Copyright © 2017 Alexandre  Vassinievski. All rights reserved.
+//
+
+import UIKit
+
+class DetailContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- eventsPin/DetailViewController.swift
+++ eventsPin/DetailViewController.swift
@@ -8,7 +8,14 @@
 
 import UIKit
 
-class DetailViewController: UIViewController , CategorySelectionDelegate{
+class DetailViewController: UIViewController , CategorySelectionDelegate, UIPageViewControllerDelegate, UIPageViewControllerDataSource{
+    
+    var pageViewController: UIPageViewController?
+    
+    var decidedPage: DetailContentViewController?
+    var inspirationPage: DetailContentViewController?
+    
+    var category : Category?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +23,18 @@ class DetailViewController: UIViewController , CategorySelectionDelegate{
         let master = self.splitViewController?.master as! MasterTableViewController
         
         master.delegate = self
+        
+        decidedPage = self.storyboard?.instantiateViewController(withIdentifier: "detailContentViewController") as? DetailContentViewController
+        
+        decidedPage?.view.backgroundColor = .red
+        
+        inspirationPage = self.storyboard?.instantiateViewController(withIdentifier: "detailContentViewController") as? DetailContentViewController
+        
+        
+        self.pageViewController?.setViewControllers([decidedPage!], direction: .forward, animated: true, completion: nil)
+        
+        self.pageViewController?.delegate = self
+        self.pageViewController?.dataSource = self
         
         // Do any additional setup after loading the view.
     }
@@ -26,16 +45,54 @@ class DetailViewController: UIViewController , CategorySelectionDelegate{
     }
     
     func categorySelected(_ category: Category) {
+        
         print(category.tag)
+        
+        self.category = category
+        
     }
-    /*
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        if viewController == inspirationPage {
+            return decidedPage
+        }
+        
+        else {
+            return nil
+        }
+    }
+    
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        if viewController == decidedPage {
+            return inspirationPage
+        }
+            
+        else {
+            return nil
+        }
+    }
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "pageControllerSegue" {
+            
+            self.pageViewController = segue.destination as? UIPageViewController
+            
+            
+            
+        }
+        
     }
-    */
+    
+    
+    
 
 }
