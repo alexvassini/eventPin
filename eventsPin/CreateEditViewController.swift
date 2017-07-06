1bbd4b9aacd5e5b434fe55e955d30f14cde89a9d
Alexandre  Vassinievski <alexvassini@gmail.com>
2017-07-06 18:43:10 -0300

    Create and layout edit view controller layout adjust

--- eventsPin/CreateEditViewController.swift
+++ eventsPin/CreateEditViewController.swift
@@ -12,22 +12,34 @@ class CreateEditViewController: UIViewController {
     
     
     @IBOutlet weak var contentUIView: UIView!
+   
+    @IBOutlet weak var photoView: UIView!
     
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    
+    @IBOutlet weak var addDataView: UIView!
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var textField: UITextField!
 
+    @IBOutlet weak var tagsView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //self.view.backgroundColor = UIColor.gray .withAlphaComponent(0.85)
-        
-        //contentUIView.layer.shadowColor = UIColor.black.cgColor
-       // contentUIView.layer.shadowOpacity = 0.6
-       // contentUIView.layer.shadowOffset = CGSize(width: 4, height: 8)
-      //  contentUIView.layer.shadowRadius = 8
-      //  contentUIView.layer.shouldRasterize = false
         contentUIView.layer.cornerRadius = 10
         
-        contentUIView.backgroundColor = Constants.generalBackgroundColor
-
+        photoView.backgroundColor = Constants.generalBackgroundColor
+        
+        navigationBar.barTintColor = DataModel.shared.settings.primaryColor
+        navigationBar.titleTextAttributes =
+            [NSForegroundColorAttributeName: UIColor.white]
+        
+        segmentedControl.tintColor = DataModel.shared.settings.secondaryColor
+        
+        
         // Do any additional setup after loading the view.
     }
 
@@ -37,6 +49,10 @@ class CreateEditViewController: UIViewController {
     }
     
     
+    @IBAction func createAndSavePins(_ sender: Any) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
     
     /*
     // MARK: - Navigation
